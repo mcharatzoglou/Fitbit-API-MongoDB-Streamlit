dc860980d8ee318d9696997971b8d878a2b93b2a
marialiatsikou <43893782+marialiatsikou@users.noreply.github.com>
2023-04-21 14:16:37 +0300

    -

--- api/streamlit_visualizations.py
+++ api/streamlit_visualizations.py
@@ -8,14 +8,28 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("heartRate.csv")
 
 df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'], format='%Y-%m-%d %H:%M:%S')
+
+# Set the datetime column as the index of the dataframe
 df.set_index('datetime', inplace=True)
 
 start_date = st.date_input("Select Start Date", df.index.min().date())
 end_date = st.date_input("Select End Date", df.index.max().date())
-filtered_df = df.loc[start_date:end_date]
+
+# Create two Streamlit time picker widgets to allow users to select the start and end times of the range
+start_time = st.time_input("Select Start Time", value=df.index.min().time())
+end_time = st.time_input("Select End Time", value=df.index.max().time())
+
+# Combine the selected date and time into a single datetime object
+start_datetime = pd.to_datetime(str(start_date) + ' ' + str(start_time))
+end_datetime = pd.to_datetime(str(end_date) + ' ' + str(end_time))
+
+# Filter the dataframe based on the selected datetime range
+filtered_df = df.loc[start_datetime:end_datetime]
+
 
 fig, ax = plt.subplots()
 ax.plot(filtered_df.index, filtered_df['heart_rate'])
+
 ax.set_xlabel("Datetime")
 ax.set_title("Heart Rate Over Time")
 fig.set_size_inches(10, 6)
