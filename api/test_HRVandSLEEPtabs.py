ae5453569929340789bbf77bee289e1e1f5fcedd
marialiatsikou <43893782+marialiatsikou@users.noreply.github.com>
2023-04-21 21:40:47 +0300

    added imports

--- api/test_HRVandSLEEPtabs.py
+++ api/test_HRVandSLEEPtabs.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 # Create tabs using beta_container
 tabs = ["Heart Rate", "Sleep"]
