1cdaadf316db2c93e538e51d84700d9394c0598f
marialiatsikou <43893782+marialiatsikou@users.noreply.github.com>
2023-04-21 19:51:39 +0300

    made it bit better

--- api/streamlit_visualizations.py
+++ api/streamlit_visualizations.py
@@ -10,10 +10,13 @@ df = pd.read_csv("heartRate.csv")
 df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'], format='%Y-%m-%d %H:%M:%S')
 df.set_index('datetime', inplace=True)
 
+st.title('My Fitbit data')
+st.markdown("### Heart Rate Data")
+        
 # Create Streamlit picker widgets 
 start_date = st.date_input("Select Start Date", df.index.min().date())
-end_date = st.date_input("Select End Date", df.index.max().date())
 start_time = st.time_input("Select Start Time", value=df.index.min().time())
+end_date = st.date_input("Select End Date", df.index.max().date())
 end_time = st.time_input("Select End Time", value=df.index.max().time())
 
 # Combine date and time into a single datetime object
@@ -32,4 +35,6 @@ ax.set_title("Heart Rate Over Time")
 ax.xaxis.set_major_formatter(plt.FixedFormatter(filtered_df.index.strftime('%Y-%m-%d %H:%M')))
 ax.tick_params(axis='x', rotation=45, labelsize=6) 
 ax.tick_params(axis='y', labelsize=6) 
+
+
 st.pyplot(fig)
